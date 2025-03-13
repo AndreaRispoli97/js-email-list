e3bbeb48de63ce04284952d3ccbb17eccd731afb
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-13 15:29:56 +0100

    add: stampa email in console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 console.log('Andrea');
+
+
+const emailList = document.getElementById('email-list');
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then((response) => {
+    const email = response.data.response;
+    console.log(email);
+
+    emailList.innerHTML += `<div class = col> ${email.element}</div>`;
+})
+.catch((error) => {
+    console.error('Errore');
+});

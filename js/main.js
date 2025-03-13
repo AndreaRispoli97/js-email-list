8ce66fa3bcd67c3f787a39181afe6e7c5f9c74a1
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-13 17:05:11 +0100

    add: aggiunto reset lista

--- js/main.js
+++ js/main.js
@@ -3,23 +3,31 @@ console.log('Andrea');
 
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const emailList = document.getElementById('email-list');
+const newEmail = document.getElementById('new-email');
 
-let emails = [];
 
-for (let i = 0; i < 10; i++) {
-    axios.get(endpoint)
-        .then((response) => {
-            let email = response.data.response;
-            console.log(email);
 
-            emails += `<li class = "col"> ${email}</li>`;
-
-            emailList.innerHTML = emails;
-
-        })
-        .catch((error) => {
-            console.error('Errore');
-        });
-
+function randomEmails(){
+   let emails = '';
+    for (let i = 0; i < 10; i++) {
+        
+        axios.get(endpoint)
+            .then((response) => {
+                let email = response.data.response;
+                console.log(email);
+    
+                emails += `<li class = "col"> ${email}</li>`;
+    
+                emailList.innerHTML = emails;
+    
+            })
+            .catch((error) => {
+                console.error('Errore');
+            });
+    
+    }
 }
 
+randomEmails();
+
+newEmail.addEventListener('click', randomEmails);
